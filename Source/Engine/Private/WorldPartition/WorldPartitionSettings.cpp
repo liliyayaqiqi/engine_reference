ced214e769c6e3531baf5d3c3a52fed7d1d2e33e
liyaqi <yaqi.li@mihoyo.com>
2026-01-13 11:34:57 +0800

    fix

--- Source/Engine/Private/WorldPartition/WorldPartitionSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WorldPartition/WorldPartitionSettings.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(WorldPartitionSettings)
-
-UWorldPartitionSettings::UWorldPartitionSettings(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{}
--- Source/Engine/Public/MaterialTypes.h
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CoreMinimal.h"
-
-UE_DEPRECATED_HEADER(5.6, "Materials/MaterialParameters.h")
